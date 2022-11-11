77cbc62568f14095da877ccfd2c5f9facedc73ac
lil climate <lilclimate@gmail.com>
2022-11-11 10:58:11 +0800

    refactor: 封装handler map

--- src/parse.ts
+++ src/parse.ts
@@ -1,22 +1,33 @@
-const map = new Map();
-map.set(Boolean, parseBoolean);
-map.set(Number, parseNumber);
-map.set(String, parseString);
+
+
 
 export function parse(options: any, key: string, args: any) {
-	const parseHandle = map.get(options[key]);
+	const handlerMap = initHandlerMap();
+	const parseHandle = handlerMap.get(options[key]);
 	return parseHandle(args, key);
 }
 function parseString(args: any, key: string) {
+	return getFlagValue(args, key);
+}
+
+function parseNumber(args: any, key: string) {
+	return Number.parseInt(getFlagValue(args, key));
+}
+
+function parseBoolean(args: any, key: string): any {
+	return args.includes(`-${key}`);
+}
+
+function getFlagValue(args: any, key: string) {
 	const flagIndex = args.indexOf(`-${key}`);
 	const value = args[flagIndex + 1];
 	return value;
 }
-function parseNumber(args: any, key: string) {
-	const flagIndex = args.indexOf(`-${key}`);
-	const value = Number.parseInt(args[flagIndex + 1]);
-	return value;
-}
-function parseBoolean(args: any, key: string): any {
-	return args.includes(`-${key}`);
+
+function initHandlerMap() {
+	const map = new Map();
+	map.set(Boolean, parseBoolean);
+	map.set(Number, parseNumber);
+	map.set(String, parseString);
+	return map;
 }
