891ed7d60f977a1309856576d6c6eb6221f666c4
lil climate <lilclimate@gmail.com>
2022-11-15 12:13:57 +0800

    feat: 定义两个最终的example

--- src/args.spec.ts
+++ src/args.spec.ts
@@ -1,4 +1,4 @@
-import {describe, test  } from "vitest";
+import {describe, expect, test  } from "vitest";
 
 //	happy path
 //	TODO: bool -l	
@@ -16,4 +16,49 @@ import {describe, test  } from "vitest";
 //	TODO: string:""
 
 describe('args', () => {
+	test.skip('should parse multi options', () => { 
+		let schema = {
+			logging: option('l', bool()), 
+			int: option('p', int()),
+			string: option('d', string()),
+		};
+		expect(parse(schema, ['-l', '-p', '8080', '-d', '/usr/logs'])).toEqual({
+			logging: true,
+			int: 8080,
+			string: "/usr/logs"
+		});
+	});
+
+	test.skip('should parse multi options in list', () => { 
+		let schema = {
+			logging: option('l', bool()), 
+			int: option('p', int()),
+			string: option('d', string()),
+		};
+		expect(parse(schema, ['-g', 'this', 'is', 'a', 'list', 'd', '1', '2', '-3', '5'])).toEqual({
+			string: ['this', 'is', 'a', 'list'],
+			int: [1, 2, -3, 5],
+		});
+	})
 });
+
+function parse(schema: any, args: string[]): any {
+}
+
+
+function option(flag: string, type: any) {
+}
+
+function bool(): any {
+	
+}
+
+function int(): any {
+	
+}
+
+function string(): any {
+	
+}
+
+
