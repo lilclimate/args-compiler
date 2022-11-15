48c5d61961976acebc9db4cf91256c2d1f358e82
lil climate <lilclimate@gmail.com>
2022-11-15 10:58:24 +0800

    fix: 修复option未匹配到flag时未调用具体类型处理

--- src/args.spec.ts
+++ src/args.spec.ts
@@ -108,7 +108,7 @@ describe('Args parser', () => {
 function option(flag ,type) { 
 	return (args) => { 
 		let index = args.indexOf(`-${flag}`);
-		if (index=== -1) return undefined;
+		if (index=== -1) return type(undefined);
 		let nextIndex = args.findIndex((v, i) => i > index && /^-[a-zA-Z-]+/.test(v) );
 		if (nextIndex === -1) nextIndex = args.length; 
 		return type(args.slice(index + 1, nextIndex));
