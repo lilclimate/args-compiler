c260cbea0ec4a3bfc68a8d644eaf03963ba37d49
lil climate <lilclimate@gmail.com>
2022-11-15 12:02:44 +0800

    feat: 任务拆分

---
+++ src/args.spec.ts
@@ -0,0 +1,19 @@
+import {describe, test  } from "vitest";
+
+//	happy path
+//	TODO: bool -l	
+//	TODO: int -p 8080
+//	TODO: string -d /usr/logs
+//	TODO: should parse multi options		  
+//	TODO: should parse multi options in list
+//	sad path
+//	TODO:	bool -l t / -l t f
+//	TODO: int -p / -p 8080 8081
+//	TODO:	string -d / -d /usr/local /usr/logs
+//	default
+//	TODO:	bool:true
+//	TODO:	int:0	
+//	TODO: string:""
+
+describe('args', () => {
+});
