0daa1676c194f590e756b5e5978c607ccb3db711
lil climate <lilclimate@gmail.com>
2022-11-15 14:01:00 +0800

    feat: bool类型实现默认值

--- src/args.spec.ts
+++ src/args.spec.ts
@@ -9,15 +9,17 @@ import {describe, expect, test  } from "vitest";
 //	should fetch empty array if no value given
 //	should fetch undefined if no flag match
 //	should call type to handle values
+//	should return true if empty array given
+// 	TODO: should return false if undefined given
 //	TODO: bool -l	
 //	TODO: int -p 8080
 //	TODO: string -d /usr/logs
 //	sad path
-//	TODO:	bool -l t / -l t f
+//	TODO:	bool -l t / -l t f / undefined
 //	TODO: int -p / -p 8080 8081
 //	TODO:	string -d / -d /usr/local /usr/logs
 //	default
-//	TODO:	bool:true
+//	bool:true
 //	TODO:	int:0	
 //	TODO: string:""
 
@@ -84,6 +86,13 @@ describe('option', () => {
 	});
 });
 
+describe('bool', () => { 
+	test('should return true if empty array given', () => { 
+		const type = bool();
+		expect(type([])).toBeTruthy();
+	})
+})
+
 function parse(schema: any, args: string[]): any {
 	const option = {};
 	for (const key of Object.keys(schema)) {
@@ -103,8 +112,11 @@ function option(flag: string, type: any) {
 	};
 }
 
-function bool(): any {
-	
+function bool(): Function {
+	return (args): boolean => {
+		if (args.length === 0) return true;
+		return false;
+	 };
 }
 
 function int(): any {
